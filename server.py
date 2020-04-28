7130fee9c0b52980011fd376c6ac8286a618c0a2
michaeldel <michaeldel@protonmail.com>
2020-04-29 00:30:40 +0200

    Add title and style

--- server.py
+++ server.py
@@ -1,6 +1,19 @@
 from bottle import route, run, template
 from requests_html import HTMLSession
 
+TEMPLATE = """
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Serif&display=swap');
+body {
+  max-width: 40rem;
+  margin: auto auto;
+  font-family: 'Noto Serif', serif;
+}
+</style>
+<h2>{{ title }}</h2>
+<article>{{ !article.html }}</article>
+"""
+
 @route('/<url:path>')
 def index(url: str):
     session = HTMLSession()
@@ -8,8 +21,10 @@ def index(url: str):
     response = session.get(url)
     response.raise_for_status()
 
+    title = response.html.find('h2.article-title', first=True).text
     article = response.html.find('div.article', first=True)
-    return template('<article>{{ !article.html }}</article>', article=article)
+
+    return template(TEMPLATE, title=title, article=article)
 
 run(host='localhost', port=8000)
 
